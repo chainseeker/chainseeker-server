f1df6f846a6a043cd1fd1baecbbdb2aa2af8bcec
Vis Virial <visvirial@gmail.com>
2021-06-29 18:58:24 +0900

    Run HTTP server before initial sync.

--- syncer/src/main.rs
+++ syncer/src/main.rs
@@ -2,25 +2,31 @@ use chainseeker_syncer::*;
 
 #[tokio::main]
 async fn main() {
+    // Read arguments.
     let args: Vec<String> = std::env::args().collect();
     if args.len() < 2 {
         println!("usage: {} COIN", args[0]);
         return;
     }
+    // Load config.
     let coin = args[1].to_string();
     let config = load_config();
+    // Create Syncer instance.
     let mut syncer = Syncer::new(&coin, &config).await;
+    // Run HTTP server.
+    let mut handles = Vec::new();
+    let server = syncer.http_server.clone();
+    handles.push(tokio::spawn(async move {
+        server.run(&config.http_ip, config.coins[&coin].http_port).await;
+    }));
+    // Do initial sync.
     syncer.initial_sync().await;
     if syncer.is_stopped().await {
         return;
     }
-    let server = syncer.http_server.clone();
-    let mut handles = Vec::new();
+    // Run syncer.
     handles.push(tokio::spawn(async move {
         syncer.run().await;
-    }));
-    handles.push(tokio::spawn(async move {
-        server.run(&config.http_ip, config.coins[&coin].http_port).await;
     }));
     // Join for the threads.
     for handle in handles.iter_mut() {
