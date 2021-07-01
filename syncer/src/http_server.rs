b05985a01352b72eb8838df6d1958f1e08964851
Vis Virial <visvirial@gmail.com>
2021-07-01 13:33:34 +0900

    Change API path `/utxo` => `/utxos`.

--- syncer/src/http_server.rs
+++ syncer/src/http_server.rs
@@ -539,8 +539,8 @@ impl HttpServer {
         let txids = txids.iter().map(|txid| txid.to_hex()).collect::<Vec<String>>();
         Ok(Self::json(&txids))
     }
-    /// `/utxo/:script_or_address` endpoint.
-    async fn utxo_handler(req: Request<Body>) -> Result<Response<Body>, Infallible> {
+    /// `/utxos/:script_or_address` endpoint.
+    async fn utxos_handler(req: Request<Body>) -> Result<Response<Body>, Infallible> {
         let server = req.data::<HttpServer>().unwrap();
         let script = Self::decode_script_or_address(req.param("script_or_address").unwrap());
         if script.is_err() {
@@ -590,7 +590,7 @@ impl HttpServer {
             .get("/api/v1/block_with_txs/:hash_or_height", Self::block_with_txs_handler)
             .get("/api/v1/block/:hash_or_height", Self::block_handler)
             .get("/api/v1/txids/:script_or_address", Self::txids_handler)
-            .get("/api/v1/utxo/:script_or_address", Self::utxo_handler)
+            .get("/api/v1/utxos/:script_or_address", Self::utxos_handler)
             .get("/api/v1/rich_list_count", Self::rich_list_count_handler)
             .get("/api/v1/rich_list/:offset/:limit", Self::rich_list_handler)
             .any(|_req| async {
