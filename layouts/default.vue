fe62b2580305278b0cbd6bd3e133dbf46c866595
Vis Virial <visvirial@gmail.com>
2021-07-03 10:10:16 +0900

    Add links for each coins.

--- layouts/default.vue
+++ layouts/default.vue
@@ -5,12 +5,14 @@
 				<nav>
 					<v-toolbar>
 						<v-toolbar-title>
-							<NuxtLink to="/" style="color: inherit">
+							<NuxtLink to="/" style="color: inherit; vertical-align: 40%;">
 								chainseeker
 							</NuxtLink>
-							<NuxtLink :to="`/${$config.coin}`" style="color: inherit" class="ml-2">
-								<small>[{{ $config.coinConfig.coin.symbol }}]</small>
-							</NuxtLink>
+							<span v-for="(config, coin) in $config.coins" :key="coin">
+								<NuxtLink v-if="coin !== 'local'" :to="`/${coin}`" style="color: #666; font-size: 80%;" class="ml-2">
+									<img :src="icons[coin]" :alt="`${config.coin.name} icon`" style="height: 5ex;" />
+								</NuxtLink>
+							</span>
 						</v-toolbar-title>
 						<div class="ml-4"><NuxtLink :to="`/${$config.coin}/rich_list`">Rich List</NuxtLink></div>
 						<div class="ml-2"><a href="https://chainseeker.docs.apiary.io/" target="_blank">REST API</a></div>
@@ -19,7 +21,7 @@
 							<v-container style="margin-top:2ex">
 								<v-row>
 									<v-col>
-										<v-text-field v-model="query" label="blockid, height, txid, address" style="width: 40em" />
+										<v-text-field v-model="query" label="blockid, height, txid, address" style="width: 30em" />
 									</v-col>
 									<v-col>
 										<v-btn type="submit">Search</v-btn>
@@ -44,8 +46,14 @@ import { Vue, Component } from 'nuxt-property-decorator';
 	middleware: ['coin'],
 })
 export default class Layout extends Vue {
-	coin: string = '';
+	icons: any = {};
 	query: string = '';
+	async fetch() {
+		for(const coin in this.$config.coins) {
+			if(coin === 'local') continue;
+			this.icons[coin] = require(`~/assets/img/coins/${coin}.png`);
+		}
+	}
 	search(e: Event) {
 		e.preventDefault();
 		this.$router.push(`/${this.$config.coin}/search/${this.query}`);
