ec28f220095593d7d1901a4c9e372965e9a11b6e
Vis Virial <visvirial@gmail.com>
2021-07-03 15:40:23 +0900

    Add site logo to the footer.

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,12 +1,21 @@
 <template>
 	<div>
 		<hr style="margin-top:5ex;" />
-		<footer>
-			<v-container>
-				<p class="my-0">Copyright &copy; chainseeker 2017-{{ new Date().getFullYear() }}. All rights reserved.</p>
-				<p class="my-0">Created &amp; mainted by <NuxtLink to="https://twitter.com/visvirial" target="_blank">@visvirial</NuxtLink>.</p>
-				<p class="my-0">Source code is available at <a href="https://github.com/chainseeker" target="_blank">our GitHub repo</a>.</p>
-			</v-container>
+		<footer class="my-2">
+			<div class="float-left">
+				<img src="~/assets/img/logo-square.png" alt="site logo" style="max-height: 100px;" />
+			</div>
+			<div class="float-left" style="padding: 15px 20px;">
+				<p class="my-0">
+					Copyright &copy; chainseeker 2017-{{ new Date().getFullYear() }}. All rights reserved.
+				</p>
+				<p class="my-0">
+					Created &amp; mainted by <NuxtLink to="https://twitter.com/visvirial" target="_blank">@visvirial</NuxtLink>.
+				</p>
+				<p class="my-0">
+					Source code is available at <a href="https://github.com/chainseeker" target="_blank">our GitHub repo</a>.
+				</p>
+			</div>
 		</footer>
 	</div>
 </template>
