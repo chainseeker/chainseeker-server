66d2fba53f57944c8c744cd42895b92a1aab874a
Vis Virial <visvirial@gmail.com>
2021-07-07 07:20:56 +0900

    Improve code coverage for `db/address_index.rs`.

--- src/db/address_index.rs
+++ src/db/address_index.rs
@@ -1,4 +1,3 @@
-use rayon::prelude::*;
 use bitcoin::{Block, Txid, Script};
 
 use super::super::*;
@@ -83,32 +82,21 @@ impl AddressIndexDB {
         let txids: Vec<Txid> = block.txdata.iter().map(|tx| {
             tx.txid()
         }).collect();
-        let mut elems = Vec::new();
         for i in 0..block.txdata.len() {
             let tx = &block.txdata[i];
             let txid = &txids[i];
             // Process vins.
             for vin in tx.input.iter() {
-                if vin.previous_output.is_null() {
-                    continue;
+                if !vin.previous_output.is_null() {
+                    // Fetch transaction from `previous_output`.
+                    self.put(&previous_utxos[previous_utxo_index].script_pubkey, txid);
+                    previous_utxo_index += 1;
                 }
-                // Fetch transaction from `previous_output`.
-                elems.push((&previous_utxos[previous_utxo_index].script_pubkey, txid));
-                previous_utxo_index += 1;
             }
             // Process vouts.
             for vout in tx.output.iter() {
-                elems.push((&vout.script_pubkey, txid));
+                self.put(&vout.script_pubkey, txid);
             }
-        }
-        let keys: Vec<AddressIndexDBKey> = elems.par_iter().map(|elem| {
-            AddressIndexDBKey {
-                script_pubkey: (*elem.0).clone(),
-                txid: (*elem.1).clone(),
-            }
-        }).collect();
-        for key in keys.iter() {
-            self.db.put(&key, &AddressIndexDBValue::new());
         }
     }
 }
@@ -134,11 +122,21 @@ mod test {
             let prev_utxos = utxo_db.process_block(&block, false);
             addr_index_db.process_block(&block, &prev_utxos);
         }
-        //print_addr_index_db(&addr_index_db);
+        print_addr_index_db(&addr_index_db);
         let mut entries_test = addr_index_db.db.iter().map(|(key, _value)| key).collect::<Vec<AddressIndexDBKey>>();
         entries_test.sort();
         let mut entries = fixtures::addr_index_db();
         entries.sort();
         assert_eq!(entries_test, entries);
+        for entry in entries.iter() {
+            let mut found = false;
+            for txid in addr_index_db.get(&entry.script_pubkey).iter() {
+                if *txid == entry.txid {
+                    found = true;
+                    break;
+                }
+            }
+            assert!(found);
+        }
     }
 }
