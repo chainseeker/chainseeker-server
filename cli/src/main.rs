616f9a91cbe6547482bace26cd7a813e806e3785
Vis Virial <visvirial@gmail.com>
2021-07-08 22:27:49 +0900

    Add test cases.

--- cli/src/main.rs
+++ cli/src/main.rs
@@ -1,30 +1,24 @@
+use std::env::args;
 use std::process::exit;
 use chainseeker::serde;
 use chainseeker::reqwest;
 
-async fn execute<S, F, E>(args: &[String], n_params: usize, exec: E)
+async fn execute<S, F, E>(args: &[String], n_params: usize, exec: E) -> Result<(), String>
     where S: serde::Serialize,
           F: std::future::Future<Output = Result<S, reqwest::Error>>,
           E: Fn() -> F,
 {
     if args.len() < n_params + 3 {
-        eprintln!("E: insufficient number of arguments.");
-        exit(3);
+        return Err("E: insufficient number of arguments.".to_string());
     }
     match exec().await {
-        Ok(result) => {
-            println!("{}", serde_json::to_string_pretty(&result).unwrap());
-        },
-        Err(err) => {
-            eprintln!("E: failed to execute: {}", err);
-            exit(2);
-        }
+        Ok(result) => println!("{}", serde_json::to_string_pretty(&result).unwrap()),
+        Err(err) => return Err(format!("E: failed to execute: {}", err)),
     }
+    Ok(())
 }
 
-#[tokio::main(flavor = "current_thread")]
-async fn main() {
-    let args: Vec<String> = std::env::args().collect();
+async fn run(args: &[String]) -> Result<(), String> {
     if args.len() < 3 {
         println!("Usage: {} COIN COMMAND [..ARGS]", args[0]);
         println!("Available COMMANDs are:");
@@ -41,7 +35,7 @@ async fn main() {
         println!("    richlistcount");
         println!("    rank ADDRESS");
         println!("    richlist OFFSET LIMIT");
-        return;
+        return Ok(());
     }
     let coin = &args[1];
     let command = &args[2];
@@ -50,7 +44,7 @@ async fn main() {
     match command.as_str() {
         "status"        => execute(&args, 0, || client.status()).await,
         "tx"            => execute(&args, 1, || client.tx(&args[i])).await,
-        "puttx"         => execute(&args, 1, || client.put_tx(args[i].clone())).await,
+        "puttx"         => execute(&args, 1, || client.put_tx(args[i].to_string())).await,
         "blocksummary"  => execute(&args, 2, || client.block_summary(args[i].parse().unwrap(), args[i+1].parse().unwrap())).await,
         "block+txids"   => execute(&args, 1, || client.block_with_txids(&args[i])).await,
         "block+txs"     => execute(&args, 1, || client.block_with_txs(&args[i])).await,
@@ -61,9 +55,95 @@ async fn main() {
         "richlistcount" => execute(&args, 0, || client.rich_list_count()).await,
         "rank"          => execute(&args, 1, || client.rich_list_addr_rank(&args[i])).await,
         "richlist"      => execute(&args, 2, || client.rich_list(args[i].parse().unwrap(), args[i+1].parse().unwrap())).await,
-        _ => {
-            eprintln!("E: invalid command: {}", command);
-            exit(1);
-        }
-    };
+        _ => Err(format!("E: invalid command: {}", command)),
+    }
+}
+
+#[tokio::main(flavor = "current_thread")]
+async fn main() {
+    if let Err(err) = run(&args().collect::<Vec<String>>()).await {
+        eprintln!("{}", err);
+        exit(1);
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    const COIN: &str = "btc";
+    const BLOCK_HASH: &str = "00000000839a8e6886ab5951d76f411475428afc90947ee320161bbf18eb6048";
+    const TXID: &str = "0e3e2357e806b6cdb1f70b54c3a3a17b6714ee1f0e68bebb44a74b1efd512098";
+    const ADDRESS: &str = "1CounterpartyXXXXXXXXXXXXXXXUWLpVr";
+    async fn test(argv: &[&str]) -> Result<(), String> {
+        run(&[vec![args().nth(0).unwrap()], argv.iter().map(|s| s.to_string()).collect()].concat()).await
+    }
+    #[tokio::test]
+    async fn no_args() {
+        assert!(test(&[]).await.is_ok());
+    }
+    #[tokio::test]
+    async fn invalid_coin() {
+        assert!(test(&["invalid", "status"]).await.is_err());
+    }
+    #[tokio::test]
+    async fn invalid_command() {
+        assert!(test(&[COIN, "invalid"]).await.is_err());
+    }
+    #[tokio::test]
+    async fn invalid_num_of_args() {
+        assert!(test(&[COIN, "tx"]).await.is_err());
+    }
+    #[tokio::test]
+    async fn status() {
+        assert!(test(&[COIN, "status"]).await.is_ok());
+    }
+    #[tokio::test]
+    async fn tx() {
+        assert!(test(&[COIN, "tx", TXID]).await.is_ok());
+    }
+    #[tokio::test]
+    async fn puttx() {
+        // TODO: create a valid transaction.
+        assert!(test(&[COIN, "puttx", "012345678"]).await.is_err());
+    }
+    #[tokio::test]
+    async fn blocksummary() {
+        assert!(test(&[COIN, "blocksummary", "0", "10"]).await.is_ok());
+    }
+    #[tokio::test]
+    async fn block_txids() {
+        assert!(test(&[COIN, "block+txids", BLOCK_HASH]).await.is_ok());
+    }
+    #[tokio::test]
+    async fn block_txs() {
+        assert!(test(&[COIN, "block+txs", BLOCK_HASH]).await.is_ok());
+    }
+    #[tokio::test]
+    async fn block() {
+        assert!(test(&[COIN, "block", BLOCK_HASH]).await.is_ok());
+    }
+    #[tokio::test]
+    async fn txids() {
+        assert!(test(&[COIN, "txids", ADDRESS]).await.is_ok());
+    }
+    #[tokio::test]
+    async fn txs() {
+        assert!(test(&[COIN, "txs", ADDRESS]).await.is_ok());
+    }
+    #[tokio::test]
+    async fn utxos() {
+        assert!(test(&[COIN, "utxos", ADDRESS]).await.is_ok());
+    }
+    #[tokio::test]
+    async fn richlistcount() {
+        assert!(test(&[COIN, "richlistcount"]).await.is_ok());
+    }
+    #[tokio::test]
+    async fn rank() {
+        assert!(test(&[COIN, "rank", ADDRESS]).await.is_ok());
+    }
+    #[tokio::test]
+    async fn richlist() {
+        assert!(test(&[COIN, "richlist", "0", "10"]).await.is_ok());
+    }
 }
