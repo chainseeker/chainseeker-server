3edf440da04036d164640e817ed329a4fb3974fc
visvirial <visvirial@users.noreply.github.com>
2021-07-09 21:16:54 +0000

    deploy: 7cc2a9c434e2f7fbad9cc666de2a77b4c482ca51

--- chainseeker_server/db/sidebar-items.js
+++ chainseeker_server/db/sidebar-items.js
@@ -1,1 +1,1 @@
-initSidebarItems({"mod":[["address_index",""],["block",""],["rich_list",""],["synced_height",""],["tx",""],["utxo",""],["utxo_server",""]]});
+initSidebarItems({"mod":[["address_index",""],["block",""],["rich_list",""],["synced_height",""],["tx",""],["utxo",""],["utxo_server",""]],"struct":[["Database",""]]});
