38d416c9b4aadea9c04d74ec5fc6cad48cb1d207
Vis Virial <visvirial@gmail.com>
2021-07-10 12:43:44 +0900

    Fix `zeromq.rs`.

--- server/src/zeromq.rs
+++ server/src/zeromq.rs
@@ -110,21 +110,19 @@ impl ZeroMQClient {
 #[cfg(test)]
 mod tests {
     use super::*;
-    const ZMQ_DIR: &str = "/tmp/chainseeker/test";
     const BLOCK_HASH: &str = "0123456789abcdef0123456789abcdef0123456789abcdef0123456789abcdef";
     // txid = caaacc4826fdf63ad0a4093400de5f1fd0c830be0724078ac039f9b29878b76f.
     const RAW_TX: &str = "0200000000010122f1294bc73da293dfe1a9088c6d26d71564bf538940c7ce9c4e6212f099c3b90000000000ffffffff011e272d0100000000160014af73f777fcd64ec6d9b22ac9e1a57e127ea169ee0247304402205fea552c7d5ed3330aa4a8b5c90a980c1d3bdc72abd13c2d7bccba91fbb978f5022027fac985cfb83339fc9227e1c653b8a824c63a49cda4f9f97d48d5c07e047608012102acc07439373cc2902d0ad6602ed6f5a1b7abdf7608d265c089160ac826a4600600000000";
     #[tokio::test(flavor = "multi_thread")]
     async fn client() {
-        std::fs::create_dir_all(ZMQ_DIR).unwrap();
-        let zmq_endpoint = format!("ipc://{}/zeromq", ZMQ_DIR);
+        const ZMQ_PORT: u16 = 8080;
         // Create a ZeroMQ server.
         let zmq_ctx = zmq::Context::new();
         let socket = zmq_ctx.socket(zmq::SocketType::PUB).unwrap();
-        socket.bind(&zmq_endpoint).unwrap();
+        socket.bind(&format!("tcp://lo:{}", ZMQ_PORT)).unwrap();
         println!("ZeroMQ server created.");
         // Run ZeroMQClient.
-        let client = ZeroMQClient::new(&zmq_endpoint);
+        let client = ZeroMQClient::new(&format!("tcp://127.0.0.1:{}", ZMQ_PORT));
         let mut rx = client.start().await;
         // Wait before ZeroMQClient is ready.
         client.wait_for_ready().await;
